821154fcfe186ef25a71b16e75b2f795f6feb8ae
Mohamedelaroussi <midmoha93@gmail.com>
2024-02-05 08:16:23 -0800

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""MyInt class module"""
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class MyInt(int):
-    """A MyInt class"""
-    def __eq__(self, other):
-        """Overides and inverts == operator"""
-        return int(self) != int(other)
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size):
+        """instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-    def __ne__(self, other):
-        """Overides and inverts != operator"""
-        return int(self) == int(other)
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
